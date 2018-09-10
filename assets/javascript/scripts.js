ceff76d57b9f6b8c1ee918211b6bbed329241987
audrey <audreyfernandez@verizon.net>
2018-09-09 17:46:41 -0700

    Contact form working

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,4 +1,14 @@
+var nodemailer = require('nodemailer');
+
 $(document).ready(function(){
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+               user: 'audreydesigns@gmail.com',
+               pass: 'kkcanon247'
+           }
+       });
 
     $("#mainContent").click(function(){
         $("#topFrame").slideUp("slow");
